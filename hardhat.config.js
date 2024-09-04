442da4a77c81291178ff76d9ddd7b02fd162b0c6
Otaiki1 <otaikisadiq2804@gmail.com>
2024-09-04 22:57:46 +0100

    refac: modify smart contracts , ready for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+    solidity: "0.8.24",
+    networks: {
+        "lisk-sepolia": {
+            chainId: 4202,
+            url: "https://rpc.sepolia-api.lisk.com", // Insert Infura url
+            accounts: [`0x${process.env.WALLET_KEY}`],
+        },
+    },
 };
--- test/MasterContract.test.js
+++ test/MasterContract.test.js
@@ -2,56 +2,94 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("MasterContract", function () {
-  let masterContract;
-  let artistContractTemplate;
-  let supporterContractTemplate;
-  let deployer;
-  let artistAddress;
-  let supporterAddress;
+    let masterContract;
+    let deployer, artist, user;
 
-  beforeEach(async function () {
-    [deployer] = await ethers.getSigners();
+    beforeEach(async function () {
+        [deployer, artist, user] = await ethers.getSigners();
 
-    const ArtistContractFactory = await ethers.getContractFactory("ArtistContract");
-    artistContractTemplate = await ArtistContractFactory.deploy();
-    await artistContractTemplate.deployed();
+        const MasterContractFactory = await ethers.getContractFactory(
+            "MasterContract"
+        );
+        masterContract = await MasterContractFactory.deploy();
+        await masterContract.deployed();
+    });
 
-    const SupporterContractFactory = await ethers.getContractFactory("SupporterContract");
-    supporterContractTemplate = await SupporterContractFactory.deploy();
-    await supporterContractTemplate.deployed();
+    it("Should deploy MasterContract", async function () {
+        expect(masterContract.address).to.not.equal(
+            ethers.constants.AddressZero
+        );
+    });
 
-    const MasterContractFactory = await ethers.getContractFactory("MasterContract");
-    masterContract = await MasterContractFactory.deploy(
-      artistContractTemplate.address,
-      supporterContractTemplate.address
-    );
-    await masterContract.deployed();
-  });
+    it("Should register an artist", async function () {
+        await masterContract.connect(artist).registerArtist("Test Artist");
+        expect(await masterContract.isArtist(artist.address)).to.be.true;
+    });
 
-  it("Should deploy MasterContract with correct templates", async function () {
-    expect(await masterContract.artistTemplate()).to.equal(artistContractTemplate.address);
-    expect(await masterContract.supporterTemplate()).to.equal(supporterContractTemplate.address);
-  });
+    it("Should register a user", async function () {
+        await masterContract.connect(user).registerUser();
+        const userData = await masterContract.users(user.address);
+        expect(userData.isRegistered).to.be.true;
+    });
 
-  it("Should initialize SupporterContract", async function () {
-    await masterContract.initializeSupporterContract();
-    const supporterContractAddress = await masterContract.supporterContractAddress();
-    expect(supporterContractAddress).to.not.equal(ethers.constants.AddressZero);
-  });
+    it("Should create a project", async function () {
+        await masterContract.connect(artist).registerArtist("Test Artist");
+        const projectName = "Test Project";
+        const goalAmount = ethers.utils.parseEther("10");
 
-  it("Should register artist correctly", async function () {
-    const name = "Artist";
-    const goalAmount = ethers.utils.parseEther("10");
-    const totalSupply = 1000;
+        await expect(
+            masterContract
+                .connect(artist)
+                .createProject(projectName, goalAmount)
+        )
+            .to.emit(masterContract, "ProjectCreated")
+            .withArgs(artist.address, ethers.utils.isAddress, 0); // The second argument checks if it's a valid address
 
-    await masterContract.registerArtist(name, goalAmount, totalSupply);
+        const artistProjects = await masterContract.getArtistProjects(
+            artist.address
+        );
+        expect(artistProjects.length).to.equal(1);
 
-    const artistContracts = await masterContract.allArtistContracts();
-    expect(artistContracts.length).to.equal(1);
+        const project = await masterContract.projects(0);
+        expect(project.name).to.equal(projectName);
+        expect(project.goalAmount).to.equal(goalAmount);
+    });
 
-    const artistContract = await ethers.getContractAt("ArtistContract", artistContracts[0]);
-    expect(await artistContract.artistName()).to.equal(name);
-    expect(await artistContract.fundingGoal()).to.equal(goalAmount);
-    expect(await artistContract.totalSupply()).to.equal(totalSupply);
-  });
+    it("Should allow user to contribute to a project", async function () {
+        await masterContract.connect(artist).registerArtist("Test Artist");
+        await masterContract.connect(user).registerUser();
+
+        await masterContract
+            .connect(artist)
+            .createProject("Test Project", ethers.utils.parseEther("10"));
+
+        const contributionAmount = ethers.utils.parseEther("1");
+        await expect(
+            masterContract
+                .connect(user)
+                .contributeToProject(0, { value: contributionAmount })
+        )
+            .to.emit(masterContract, "UserContributed")
+            .withArgs(user.address, 0);
+
+        const userContributions = await masterContract.getUserContributions(
+            user.address
+        );
+        expect(userContributions.length).to.equal(1);
+        expect(userContributions[0]).to.equal(0); // First project has tokenId 0
+    });
+
+    it("Should not allow unregistered user to contribute", async function () {
+        await masterContract.connect(artist).registerArtist("Test Artist");
+        await masterContract
+            .connect(artist)
+            .createProject("Test Project", ethers.utils.parseEther("10"));
+
+        const contributionAmount = ethers.utils.parseEther("1");
+        await expect(
+            masterContract
+                .connect(user)
+                .contributeToProject(0, { value: contributionAmount })
+        ).to.be.revertedWith("User not registered");
+    });
 });
